6118186b46b40a8b3776b36734d173be28965b8f
EFL <asstudioefl@gmail.com>
2024-07-29 16:39:49 +0800

    更新 dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -56,7 +56,8 @@ def update_dns_record(record_id, name, cf_ip):
 
     response = requests.put(url, headers=headers, json=data)
 
-    if response.status_code == 200:
+    code = response.status_code
+    if code == 200:
         print(f"cf_dns_change success: ---- Time: " + str(
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + " ---- ip：" + str(cf_ip))
         return "ip:" + str(cf_ip) + "解析" + str(name) + "成功"
@@ -64,7 +65,7 @@ def update_dns_record(record_id, name, cf_ip):
         traceback.print_exc()
         print(f"cf_dns_change ERROR: ---- Time: " + str(
             time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + " ---- MESSAGE: " + str(e))
-        return "ip:" + str(cf_ip) + "解析" + str(name) + "失败"
+        return "ip:" + str(cf_ip) + "解析" + str(code) + "失败"
 
 # 消息推送
 def push_plus(content):
